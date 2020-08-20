79417f9e9586734974c65d457fc8636575641e14
emreturgutce <emrebursalee@gmail.com>
2020-08-20 12:14:07 +0300

    send method converted into json

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(require('cors')());
 const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
-  res.send(commentsByPostId[req.params.id] || []);
+  res.json(commentsByPostId[req.params.id] || []);
 });
 
 app.post('/posts/:id/comments', (req, res) => {
@@ -20,7 +20,7 @@ app.post('/posts/:id/comments', (req, res) => {
 
   commentsByPostId[req.params.id] = comments;
 
-  res.status(201).send(comments);
+  res.status(201).json(comments);
 });
 
 app.listen(4001, () => console.log('App is running on port 4001'));
